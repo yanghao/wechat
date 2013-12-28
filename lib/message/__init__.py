2069bc0141dc391d9847f75dea63b819a258568f
Hua Yanghao <yanghao.hua@intel.com>
2013-12-28 16:53:24 +0800

    all message type from server to user is supported

--- lib/message/__init__.py
+++ lib/message/__init__.py
@@ -8,6 +8,8 @@ from mako.lookup import TemplateLookup
 
 message_lookup = TemplateLookup(directories=[_p])
 
+class MessageError(Exception): pass
+
 class Message:
     def __init__(self, to_user, from_user, content):
         self.to_user = to_user
@@ -30,20 +32,66 @@ class TextContent(Content):
 
 class NewsContent(Content):
     msg_type = 'news'
+    def __init__(self, news_list):
+        self.news_count = len(news_list)
+        for item in news_list:
+            self.validate(item)
+        self.news = news_list
+
+    def validate(self, item):
+        if type(item) != dict:
+            raise MessageError("Only dict is accepted")
+        keys = item.keys()
+        for i in ['title', 'desc', 'pic_url', 'url']:
+            if i not in keys:
+                raise MessageError("key not found in message item: %s" % i)
 
 class PhotoContent(Content):
     msg_type = 'image'
+    def __init__(self, media_id):
+        self.media_id = media_id
 
 class VoiceContent(Content):
     msg_type = 'voice'
+    def __init__(self, media_id):
+        self.media_id = media_id
 
 class MusicContent(Content):
     msg_type = 'music'
+    def __init__(self, title, media_id, desc, url, url_hq=None):
+        self.title = title
+        self.media_id = media_id
+        self.desc = desc
+        self.url = url
+        self.url_hq = url_hq
 
 class VideoContent(Content):
     msg_type = 'video'
+    def __init__(self, media_id, media_title, media_desc):
+        self.media_id = media_id
+        self.media_title = media_title
+        self.media_desc = media_desc
 
 if __name__ == '__main__':
+    print("=======================================================")
     text_content = TextContent("I love this game ...")
     msg = Message('xiaoyezi', 'hua', text_content)
-    print str(msg)
+    print(str(msg))
+    print("=======================================================")
+    news = {'title': 'test titile', 'desc': "test description",
+                 'pic_url': 'test pic url', 'url': 'url ...'}
+    news_list = [news, news, news]
+    news_content = NewsContent(news_list)
+    print(str(Message('xiaoyezi', 'hua', news_content)))
+    print("=======================================================")
+    photo = PhotoContent(3)
+    print(str(Message('xiaoyezi', 'hua', photo)))
+    print("=======================================================")
+    voice = VoiceContent(888)
+    print(str(Message('xiaoyezi', 'hua', voice)))
+    print("=======================================================")
+    music = MusicContent("music title", 77, "music description", 'url low quality', 'url_hq ...')
+    print(Message('xiaoyezi', 'hua', music))
+    print("=======================================================")
+    video = VideoContent(8, "test media title", "This is a test only video ...")
+    print(str(Message('xiaoyezi', 'hua', video)))
--- lib/message/template/news.mako
+++ lib/message/template/news.mako
@@ -1,14 +1,14 @@
 <%inherit file="base.mako" />
 
 <%block name="content">
-    <ArticleCount>${msg.content.article_count}</ArticleCount>
+    <ArticleCount>${msg.content.news_count}</ArticleCount>
     <Articles>
-    % for i in range(msg.article_count):
+    % for i in range(msg.content.news_count):
     <item>
-        <Title><![CDATA[${msg.content.article[i]['title']}]]></Title>
-        <Description><![CDATA[${msg.content.article[i]['desc']}]]></Description>
-        <PicUrl><![CDATA[${msg.content.article[i]['pic_url']}]]></PicUrl>
-        <Url><![CDATA[${msg..content.article[i]['url']}]]></Url>
+        <Title><![CDATA[${msg.content.news[i]['title']}]]></Title>
+        <Description><![CDATA[${msg.content.news[i]['desc']}]]></Description>
+        <PicUrl><![CDATA[${msg.content.news[i]['pic_url']}]]></PicUrl>
+        <Url><![CDATA[${msg.content.news[i]['url']}]]></Url>
     </item>
     % endfor
     </Articles>
